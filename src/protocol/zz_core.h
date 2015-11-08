9ea3a56bf145014d53e58e0a278a8426b05c52e1
Aram <aramamv@mail.ru>
2015-11-08 16:18:04 +0400

    Upedating core file

---
+++ src/protocol/zz_core.h
@@ -0,0 +1,76 @@
+/******************************************************************************/ 
+/*  This is the open source project which has GNU GNU GENERAL PUBLIC LICENSE  */
+/*	        The developers are Aram Hakhverdyan and Levon Gasparyan.          */
+/******************************************************************************/
+
+/*
+	File: 
+		zz_core.h
+	Abstract:
+		Defines the main protocol concepst to work with the low-level layer
+*/
+
+#ifndef ZZ_CORE_H
+#define ZZ_CORE_H
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//	Includes
+//
+//
+//  STD includes
+//
+#include <stdexcept>
+#include <new>
+
+//
+//	Qt includes
+//
+#include <QObject>
+#include <QMetaType>
+#include <QPair>
+#include <QString>
+#include <QList>
+#include <QMap>
+#include <QVector>
+#include <QSet>
+#include <QHash>
+#include <QStack>
+#include <QQueue>
+#include <QByteArray>
+#include <QBitArray>
+#include <QLinkedList>
+#include <QStringList>
+#include <QVariant>
+#include <QDateTime>
+#include <QDataStream>
+#include <QTextStream>
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+namespace zz { // ZavZag :P
+////////////////////////////////////////////////////////////////////////////////
+
+//
+//	Request Types
+//
+enum class ERequestType
+{
+	Undefined = 0,
+	Register,
+	Login,
+	Logout,
+	Lookup,
+	Friend,
+	ShowMessaging,
+	SettingsChanged,
+	SendMessage,
+	Unregister
+};
+
+////////////////////////////////////////////////////////////////////////////////
+} // namespace zz
+////////////////////////////////////////////////////////////////////////////////
+
+#endif // ZZ_CORE_H
+/* end of file */
