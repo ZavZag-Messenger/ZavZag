488b0fddd42628a538e9256e379549e3504a32c8
Aram <aramamv@mail.ru>
2015-11-16 00:01:46 +0400

    Client common solution integration

---
+++ src/client/GeneratedFiles/qrc_cl_cmainwindow.cpp
@@ -0,0 +1,30 @@
+/****************************************************************************
+** Resource object code
+**
+** Created: Sun Nov 15 22:17:44 2015
+**      by: The Resource Compiler for Qt version 4.7.2
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <QtCore/qglobal.h>
+
+QT_BEGIN_NAMESPACE
+
+QT_END_NAMESPACE
+
+
+int QT_MANGLE_NAMESPACE(qInitResources_cl_cmainwindow)()
+{
+    return 1;
+}
+
+Q_CONSTRUCTOR_FUNCTION(QT_MANGLE_NAMESPACE(qInitResources_cl_cmainwindow))
+
+int QT_MANGLE_NAMESPACE(qCleanupResources_cl_cmainwindow)()
+{
+    return 1;
+}
+
+Q_DESTRUCTOR_FUNCTION(QT_MANGLE_NAMESPACE(qCleanupResources_cl_cmainwindow))
+
